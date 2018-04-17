b2d784b0115f92ae60fd6acc1ef9c21cdf0f7dbf
MARK FONTENOT <watkinsjake1997@gmail.com>
2018-04-16 21:12:28 -0500

    adding a stemmer implemtation from porter2

--- Sprint6/main.cpp
+++ Sprint6/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "fileparser.h"
+#include "porter2_stemmer.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
 
-    FileParser t1;
-    t1.test();
-    return 0;
+
+    string test = "running";
+    string test1 = "jacked";
+    Porter2Stemmer::stem(test);
+    Porter2Stemmer::stem(test1);
+    cout << test << endl;
+    cout << test1 << endl;
+
 }
