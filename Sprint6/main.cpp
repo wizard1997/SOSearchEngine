e5ce2da9140b657883673ca4e84695c7689c8e6b
Addison Kimball <addison@akimball.com>
2018-04-17 21:53:03 -0500

    merge addy

--- Sprint6/main.cpp
+++ Sprint6/main.cpp
@@ -30,23 +30,5 @@ int main(int argc, char* argv[]) {
             cout << "Invalid flag, check arguments" << endl;
             return 1;
         }
-=======
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
-
-    std::ios_base::sync_with_stdio(false);
-    FileParser fp1;
-    fp1.test();
-
-    std::string test = "running";
-    std::string test1 = "JAckE/@@d";
-    Porter2Stemmer::stem(test);
-    Porter2Stemmer::trim(test1);
-    Porter2Stemmer::stem(test1);
-    std::cout << test << std::endl;
-    std::cout << test1 << std::endl;
-
-    fp1.parseQuestionFile(argv[1]);
->>>>>>> ffa6b9294713d97af3fbe18fff8dfdcd8409b682
 }
