da42de7fdfc4b59b63416f4aadfaaa2f2c1658a5
MARK FONTENOT <watkinsjake1997@gmail.com>
2018-05-07 04:44:21 -0500

    pushing

--- Sprint6/tests.cpp
+++ Sprint6/tests.cpp
@@ -12,7 +12,17 @@
 TEST_CASE("AVL Tree", "[AVLTree]") {
 
 
-    //std::cout << "test" << std::endl;
+    AVLTree<int> tree;
+    tree.insert(55);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(17);
+    tree.insert(9);
+    tree.insert(78);
+    tree.insert(42);
+    tree.insert(3);
+
+    REQUIRE (tree.getCount() == 8);
 
 }
 TEST_CASE("Hash Table", "[HashTable]") {
